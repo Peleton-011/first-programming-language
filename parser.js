e1b6ba06e9c35927cc384b7c0e1b44f36d8d1716
Nico Diaz <nicodiazan@gmail.com>
2024-05-20 13:05:16 +0200

    Add scaffolding for the basic functionality of the parser

--- parser.js
+++ parser.js
@@ -2,28 +2,61 @@ import { EaselError } from "./stdlib";
 import { TOKENS } from "./lexer";
 
 export class Parser {
-    constructor(tokens) {
-        this.tokens = tokens;
-        this.ast = []; //Abstract syntax tree
-        this.current = 0;
-    }
+	constructor(tokens) {
+		this.tokens = tokens;
+		this.ast = []; //Abstract syntax tree
+		this.current = 0;
+	}
 
-    error(token, msg) {
-        throw new EaselError(`Syntax error on ${token.line}:${token.column}: ${msg}`);
-    }
+	error(token, msg) {
+		throw new EaselError(
+			`Syntax error on ${token.line}:${token.column}: ${msg}`
+		);
+	}
 
-    peek() {
-        if(this.current >= this.tokens.length) return null;
-        return this.tokens[this.current];
-    }
+	peek() {
+		if (this.current >= this.tokens.length) return null;
+		return this.tokens[this.current];
+	}
 
-    peekType() {
-        if(this.current >= this.tokens.length) return null;
-        return this.tokens[this.current].type;
-    }
+	peekType() {
+		if (this.current >= this.tokens.length) return null;
+		return this.tokens[this.current].type;
+	}
 
-    parse() {
-        while(this.peekType() !== "EOF") continue;
-        return this.ast;
-    }
+	eat(type) {
+		if (this.peekType() === type) {
+			return this.tokens[this.current++];
+		}
+		this.error(
+			this.peek(),
+			`Expected ${type} but got ${this.peekType().toString()}`
+		);
+	}
+
+	simple() {
+		return this.eat(TOKENS.Identifier);
+	}
+
+	expression() {
+		const left = this.simple();
+		return left;
+	}
+
+	statement() {
+		const next = this.peek();
+
+		switch (next.type) {
+			case TOKENS.Identifier:
+				return this.expression();
+
+			default:
+				return this.expression();
+		}
+	}
+
+	parse() {
+		while (this.peekType() !== "EOF") continue;
+		return this.ast;
+	}
 }
