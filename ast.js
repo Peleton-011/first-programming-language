03687e93937a384785176a120a2e6c2d0797ca49
Nico Diaz <nicodiazan@gmail.com>
2024-05-20 13:16:28 +0200

    Add an Array class to the AST

--- ast.js
+++ ast.js
@@ -1,10 +1,18 @@
 export class Literal {
-    constructor(value) {
-        this.value = value;
-        this.type = "Literal";
-    }
+	constructor(value) {
+		this.value = value;
+		this.type = "Literal";
+	}
+}
+
+export class Array {
+	constructor(items) {
+		this.value = items;
+		this.type = "Array";
+	}
 }
 
 export default {
-    Literal
-}
+	Literal,
+	Array,
+};
