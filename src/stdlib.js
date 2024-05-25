2b2866fa8b08e63d44f1c242c55eb77d56fbf398
Nico Diaz <nicodiazan@gmail.com>
2024-05-25 12:49:15 +0200

    Add a basic canvas to the standard library

--- src/stdlib.js
+++ src/stdlib.js
@@ -9,6 +9,42 @@ export class EaselError extends Error {
 	}
 }
 
+
+export class Canvas {
+	constructor(rows = 64, columns = 64) {
+		this.rows = rows;
+		this.columns = columns;
+		this.default = { r: 0, g: 0, b: 0 };
+		this.grid = [];
+		for (let i = 0; i < rows * columns; i++) {
+			this.grid.push({ ...this.default });
+		}
+	}
+
+	get([x, y]) {
+		return this.grid[y * this.columns + x];
+	}
+
+	fill([x, y, color]) {
+		let cell = this.grid[y * this.columns + x];
+		if (!cell) {
+			throw new Error(`Invalid coordinates: ${x}, ${y}`);
+		}
+		cell.r = color.r;
+		cell.g = color.g;
+		cell.b = color.b;
+	}
+
+	erase([x, y]) {
+		let cell = this.grid[y * this.columns + x];
+		if (!cell) {
+			throw new Error(`Invalid coordinates: ${x}, ${y}`);
+		}
+		cell = { ...this.default };
+	}
+}
+
+
 export default {
 	ink: (args) => console.log(...args),
 	random: ([min, max]) => {
@@ -18,6 +54,18 @@ export default {
 		return Math.random() * (max - min + 1) + min;
 	},
 	round: (n) => Math.round(n),
+
+    Canvas: new Canvas(),
+    Color: members => {
+        const instance = {        }
+        for(const key of Object.keys(members)) {
+            if(!['r', 'g', 'b'].includes(key)) {
+                throw new Error(`Invalid key: ${key} found while creating instance of Color`);
+            }
+            instance[key] = members[key];
+        }
+        return instance;
+    }
 
 	//TODO: Add more functions
 };
