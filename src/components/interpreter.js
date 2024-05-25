21750e6590d89a78133c06e5e8ce7e74c57cdcde
Nico Diaz <nicodiazan@gmail.com>
2024-05-25 11:06:18 +0200

    Start adding some necessary helper functions

--- src/components/interpreter.js
+++ src/components/interpreter.js
@@ -7,4 +7,27 @@ export default class Interpreter {
 	}
 
 	run(ast, scope) {}
+
+	inScope(scope, name) {
+		return Object.keys(scope).includes(name);
+	}
+
+	evaluate(value, scope) {}
+
+	execute(node, scope) {
+		switch (node.constructor.name) {
+			case Ast.Variable:
+			case Ast.Set:
+			case Ast.StructStatement:
+			case Ast.FunctionStatement:
+			case Ast.ReturnStatement:
+			case Ast.ForStatement:
+			case Ast.WhileStatement:
+			case Ast.ConditionalStatement:
+			default:
+				this.evaluate(node, scope);
+		}
+
+		return scope;
+	}
 }
