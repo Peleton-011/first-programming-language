4ac272accf3bad97d491a39df0890ee5dd35b249
Nico Diaz <nicodiazan@gmail.com>
2024-05-25 11:51:24 +0200

    Add some functions to the standard library

--- src/stdlib.js
+++ src/stdlib.js
@@ -1,10 +1,23 @@
 export class EaselError extends Error {
-    constructor(message) {
-        super();
-        this.message = message;
-    }
+	constructor(message) {
+		super();
+		this.message = message;
+	}
 
-    toString() {
-        return this.message;
-    }
+	toString() {
+		return this.message;
+	}
 }
+
+export default {
+	ink: (args) => console.log(...args),
+	random: ([min, max]) => {
+		if (min >= 0 && max <= 1) {
+			return Math.random();
+		}
+		return Math.random() * (max - min + 1) + min;
+	},
+	round: (n) => Math.round(n),
+
+	//TODO: Add more functions
+};
